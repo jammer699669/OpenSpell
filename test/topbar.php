1bd2614bd3a93ae2494cc046c168e1189dab1683
James Blanchette <jamesblanchette@itechecom.com>
2018-06-09 13:00:48 +1000

    Add files via upload

---
+++ test/topbar.php
@@ -0,0 +1,23 @@
+<!-- Sub Navigation -->
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="menu">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="aboutus.php">THe Project</a></li>
+      <li><a href="download.php">Download</a></li>
+    </ul>
+  </div>
+
+  <div class="top-bar-right">
+    <ul class="menu">
+
+      <li>
+        <form method="post" action="index.php">
+          <input name="doitword" type="hidden" value="1"/>
+        <input name='word' type="text" placeholder="Search"></li>
+      <li><button type="submit" class="button">Search</button></form></li>
+
+    </ul>
+  </div>
+</div>
+<!-- /Sub Navigation -->
